020501f7ee04ec1b1b3b5e45a44065cbee1a7f27
Angelo Paolella <ap2099903@gmail.com>
2022-12-14 09:48:41 -0800

    show all cars working

--- app/src/main/java/lasalle/midterm/cargo/AllCars.java
+++ app/src/main/java/lasalle/midterm/cargo/AllCars.java
@@ -1,20 +1,34 @@
 package lasalle.midterm.cargo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.squareup.picasso.Picasso;
+
+import Model.Car;
+
 
 public class AllCars extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth checkAuth;
-    Button btnAddVehicule;
+    FirebaseListAdapter adapter;
+    Button btnAddVehicule,btnHistory,btnWishlist,btnLogout,btnShowallrentals;
+    ListView listAllCars;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,23 +43,85 @@ public class AllCars extends AppCompatActivity implements View.OnClickListener {
     private void initalize() {
 
         FirebaseUser currentUser = checkAuth.getCurrentUser();
-
+        // initialise all buttons
         btnAddVehicule = findViewById(R.id.btnAddCar);
         btnAddVehicule.setOnClickListener(this);
+        btnHistory = findViewById(R.id.btnHistory);
+        btnHistory.setOnClickListener(this);
+        btnWishlist= findViewById(R.id.btnWishlist);
+        btnWishlist.setOnClickListener(this);
+        btnLogout= findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(this);
+        btnShowallrentals = findViewById(R.id.btnShowRentals);
+        btnShowallrentals.setOnClickListener(this);
 
-            if (!currentUser.getEmail().equals("admin@gmail.com")){
+        // initialise list view
+        listAllCars = (ListView) findViewById(R.id.listAllCars);
+
+        Query query = FirebaseDatabase.getInstance().getReference().child("Car");
+        FirebaseListOptions<Car> options = new FirebaseListOptions.Builder<Car>().setLayout(R.layout.element).setQuery(query,Car.class).setLifecycleOwner(this).build();
+
+        adapter = new FirebaseListAdapter(options) {
+            @Override
+            protected void populateView(@NonNull View v, @NonNull Object model, int position) {
+                TextView carid = v.findViewById(R.id.elementId);
+                TextView models = v.findViewById(R.id.elementModel);
+                TextView modelyear = v.findViewById(R.id.elementModelYear);
+                ImageView photo = v.findViewById(R.id.elementPhoto);
+
+                Car car =(Car) model;
+                carid.setText("Car Id: "+Integer.toString(car.getCarId()));
+                models.setText("Car Model: "+car.getVehicule());
+                modelyear.setText("Model Year: "+Integer.toString(car.getModelYear()));
+                Picasso.get().load(car.getCarPic()).into(photo);
+                
+            }
+        };
+            listAllCars.setAdapter(adapter);
+
+            if (currentUser.getEmail().equals("admin@gmail.com")){
                 btnAddVehicule.setVisibility(View.VISIBLE);
-                Log.d("ButonShown", currentUser.getEmail());
+                btnShowallrentals.setVisibility(View.VISIBLE);
+                //Log.d("ButonShown", currentUser.getEmail());
             }else{
                 btnAddVehicule.setVisibility(View.INVISIBLE);
-                Log.d("ButtonHidden", currentUser.getEmail());
+                btnShowallrentals.setVisibility(View.INVISIBLE);
+               // Log.d("ButtonHidden", currentUser.getEmail());
         }
     }
 
 
     @Override
     public void onClick(View view) {
-        Intent i = new Intent(this,AddRentalCar.class);
-        startActivity(i);
+
+        int id= view.getId();
+
+        switch (id){
+            case R.id.btnAddCar:
+                Intent a = new Intent(this,AddRentalCar.class);
+                startActivity(a);
+                break;
+            case R.id.btnHistory:
+                //TODO History of rented vechicules per user
+                Intent h = new Intent(this,History.class);
+                startActivity(h);
+                break;
+            case R.id.btnWishlist:
+                // TODO wishlisted cars
+                Intent w = new Intent(this,Wishlist.class);
+                startActivity(w);
+                break;
+            case R.id.btnShowRentals:
+                // TODO show rentals for all users (admin feature)
+                Intent i = new Intent(this,ShowAllRentals.class);
+                startActivity(i);
+                break;
+            case R.id.btnLogout:
+                FirebaseAuth.getInstance().signOut();
+                Log.d("sign out","Sign out successful");
+                finish();
+                break;
+        }
+
     }
 }
